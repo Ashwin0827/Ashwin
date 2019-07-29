8016b22ae38f3570e6a210eecb4dc7236495fad8
Ashwin0827 <52060514+Ashwin0827@users.noreply.github.com>
2019-07-29 16:11:43 +0530

    Add files via upload

---
+++ demo1/src/main/java/com/first/demo/PageController.java
@@ -0,0 +1,12 @@
+package com.first.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/faqs")
+    public String first(){
+        return "index";
+    }
+}
